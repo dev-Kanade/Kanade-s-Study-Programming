5076803f6db647b7ac96d1f60d21fba030871c61
Rion <kanade.miyachi@gmail.com>
2025-10-05 19:40:28 +0900

    オブジェクト

--- JavaScript/Express.js
+++ JavaScript/Express.js
@@ -1,3 +1,9 @@
 /* ===== Express.jsの勉強です() ===== */
 const express = require("express");
 const app = express();
+const port = 3000;
+
+const studyinfo ={
+    "title":"Express.jsの勉強です^^"
+}
+app.get("/api/studyinfo")
